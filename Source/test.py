bf70fd9d45c985023e896969b567496419e2a194
harmony.sys <71132236+Harmonyasha@users.noreply.github.com>
2024-09-21 22:06:45 +0300

    Delete Source/test.py

--- Source/test.py
+++
@@ -1,9 +0,0 @@
-import os
-path = os.getcwd().replace("\\Source","")+"./AssetCache"
-files = [f for f in os.listdir(path)]
-for f in files:
-    if f.endswith(".rbxm"):
-        file_without_extension = f.replace(".rbxm", "")
-        if file_without_extension in files:
-            os.remove(os.path.join(path, file_without_extension))
-        os.rename(os.path.join(path, f), os.path.join(path, file_without_extension))
