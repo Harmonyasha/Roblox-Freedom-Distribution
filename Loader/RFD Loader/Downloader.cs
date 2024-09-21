ed682a8cc7ce7b8cb7702cc45eb454ddbefa1d68
harmony.sys <71132236+Harmonyasha@users.noreply.github.com>
2024-09-21 22:19:03 +0300

    Joiner + Auto download

---
+++ Loader/RFD Loader/Downloader.cs
@@ -0,0 +1,107 @@
+﻿//using SevenZipExtractor;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeadCons_loader
+{
+    internal class Downloader
+    {
+        internal static async Task RFD()
+        {
+            if (!File.Exists("RFD.exe"))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("RFD.exe not detected");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Starting to download");
+
+                try
+                {
+                    using (HttpClient client = new HttpClient())
+                    {
+                        byte[] r = await client.GetByteArrayAsync("https://raw.githubusercontent.com/OwlUniversal/RFD/main/RFD.exe");
+                        File.WriteAllBytes("RFD.exe", r);
+                    }
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("RFD.exe successfully downloaded");
+                }
+                catch (Exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Cannot download RFD.exe from GitHub, try again later");
+                    await Task.Delay(10000);
+                    return;
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("RFD.exe detected");
+            }
+
+        }
+/*
+        internal static async Task Roblox()
+        {
+            if (!Directory.Exists("Roblox"))
+            {
+                string url = "https://github.com/Windows81/Roblox-Filtering-Disabled/releases/download/2024-06-12T0810Z/v463.Player.7z";
+                string currentDirectory = Directory.GetCurrentDirectory();
+                string downloadPath = Path.Combine(Path.GetTempPath(), "v463.Player.7z");
+
+
+                string extractPath = Path.Combine(currentDirectory, "Roblox");
+                // Загрузка файла
+                using (HttpClient client = new HttpClient())
+                {
+                    Console.WriteLine("Downloading Roblox Player v463...");
+                    byte[] data = await client.GetByteArrayAsync(url);
+                    File.WriteAllBytes(downloadPath, data);
+                    Console.WriteLine("Download complete.");
+                }
+
+                Console.WriteLine("Unpacking file...");
+                Directory.CreateDirectory(extractPath);
+
+                using (ArchiveFile archiveFile = new ArchiveFile(downloadPath))
+                {
+                    int count = archiveFile.Entries.Count;
+                    int i = 0;
+                    Parallel.ForEach(archiveFile.Entries, entry =>
+                    {
+                        int index = System.Threading.Interlocked.Increment(ref i); // Обеспечение корректного счёта
+
+                        Console.Write($"\r[{index}/{count}]"); // Обновление прогресса
+
+                        if (!entry.IsFolder)
+                        {
+                            string destinationPath = Path.Combine(extractPath, entry.FileName);
+
+                            // Создание каталога, если его нет
+                            Directory.CreateDirectory(Path.GetDirectoryName(destinationPath));
+
+                            // Распаковка файла
+                            entry.Extract(destinationPath);
+                        }
+                    });
+                }
+                Console.WriteLine($"Unpacking complete. Files extracted to: {extractPath}");
+
+                // Удаление временного файла
+                File.Delete(downloadPath);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Roblox folder detected");
+            }
+
+        }
+   */
+    }
+}
---
+++ Loader/RFD Loader/Program.cs
@@ -0,0 +1,64 @@
+﻿using DeadCons_loader;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeadCons_loader_4._8
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Title = "DeadCons joiner";
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            await Downloader.RFD();
+            //await Downloader.Roblox();
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = "RFD.exe",
+                Arguments = @"player -rh ""46.8.232.164"" -rp 2005 -wp 2008 -u ""923423443""",
+                UseShellExecute = true,
+                RedirectStandardOutput = false, 
+                RedirectStandardError = false,  
+                CreateNoWindow = false 
+            };
+
+            try
+            {
+                using (Process process = Process.Start(startInfo))
+                {
+                    if (process == null)
+                    {
+                        Console.WriteLine("Failed to start process.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Process started successfully.");
+                        process.WaitForExit();
+                        Console.WriteLine("Process exited.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred while starting the process:");
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Process exited.");
+
+
+            await Task.Delay(10000);
+
+        }
+    }
+}
